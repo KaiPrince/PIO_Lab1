f6a246d753286ac747c91498a5f725ea3da65c17
Kai Prince <34746763+KaiPrince@users.noreply.github.com>
2021-02-02 15:37:16 -0500

    Add Searching

--- main.py
+++ main.py
@@ -5,54 +5,94 @@
  * Date: Tue, Feb 02, 2021
  * Description: This program compares sorting algorithms.
 """
-import time
+import cProfile
+import io
+import pstats
 from typing import Callable
+
+import numpy
+from func_timeout import func_set_timeout
+from func_timeout.exceptions import FunctionTimedOut
+
 from bogo_sort import bogo_sort
 from bubble_sort import bubble_sort
 from radix_sort import radix_sort
-from func_timeout import func_set_timeout
-from func_timeout.exceptions import FunctionTimedOut
-import numpy
-import cProfile
+from binary_search import binary_search
 
 
 def main():
+    # Sorting
     for algorithm, sizes in [
         (bogo_sort, [5, 10, 20]),
         (bubble_sort, [100, 1000, 10000]),
         (radix_sort, [100, 1000, 10000, 100000, 1000000]),
     ]:
         for size in sizes:
-            test_func = setup_test(algorithm, size)
-            results = run_test(test_func)
+            test_data, run_func = setup_sort_test(algorithm, size)
+            results = run_test(run_func, test_data)
+            report_results(algorithm, size, results)
+
+    # Searching
+    for algorithm, sizes in [(binary_search, [100, 1000, 10000, 100000, 1000000])]:
+        for size in sizes:
+            sorted_test_data, run_func = setup_search_test(algorithm, size)
+            results = run_test(run_func, sorted_test_data)
             report_results(algorithm, size, results)
 
 
-def setup_test(algorithm, size):
+def setup_sort_test(algorithm, size):
+    test_data = get_test_data(size)
+
+    def run_func(data):
+        return algorithm(data)
+
+    return (test_data, run_func)
+
+
+def setup_search_test(algorithm, size):
+    test_data = get_test_data(size)
+
+    # Sort data before test
+    sorted_test_data = radix_sort(test_data)
+
+    def run_func(data):
+        return algorithm(data, test_data[0])
+
+    return (sorted_test_data, run_func)
+
+
+def get_test_data(size):
     test_data = numpy.random.randint(0, 1000, size)
 
-    @func_set_timeout(10)
-    def func():
-        with cProfile.Profile() as profile:
-            profile.runcall(algorithm, test_data.copy())
-
-        profile.print_stats()
-
-    return func
+    return test_data
 
 
-def run_test(func):
+def run_test(func, test_data):
     results = []
+
     for _ in range(3):
-        start = time.process_time()
+        test_data_copy = test_data.copy()
+
+        @func_set_timeout(10)
+        def do_test():
+            with cProfile.Profile() as profile:
+                profile.runcall(func, test_data_copy)
+                # profile.print_stats()
+
+                s = io.StringIO()
+                ps = pstats.Stats(profile, stream=s)
+                ps.print_stats()
+
+            return s.getvalue()
 
         try:
-            func()
+            output = do_test()
+
+            result = output.split("\n")[0].strip()
         except FunctionTimedOut:
-            print("Test timed out.")
+            result = "Test timed out."
             pass
 
-        result = time.process_time() - start
         results.append(result)
     return results
 
